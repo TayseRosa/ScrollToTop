982c18fae9ed3227a8ac5c3293f8e8085172f24d
Tayse Rosa <tayse_rosa@yahoo.com.br>
2021-04-05 08:40:30 -0300

    Finalizado ScrollToTop

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function subirTela(){
+function upScroll(){
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
+
+function statusScroll(){
+    if( window.scrollY === 0 ){
+        // Ocultar botao
+        document.querySelector('.scrollButton').style.display = 'none';
+    }else{
+        // mostrar botao
+        document.querySelector('.scrollButton').style.display = 'block';
+    }
+}
+// setInterval(statusScroll, 1000)
+window.addEventListener('scroll', statusScroll);
